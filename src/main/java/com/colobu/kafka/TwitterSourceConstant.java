ca9c517fe98dc1911e0897d24a988e225d550667
niksisley <niksisley@gmail.com>
2016-11-27 23:26:32 +0100

    Initial commit

---
+++ src/main/java/com/colobu/kafka/TwitterSourceConstant.java
@@ -0,0 +1,11 @@
+package com.colobu.kafka;
+
+/**
+ * Created by nico on 27/11/16.
+ */
+public class TwitterSourceConstant {
+    public static final String CONSUMER_KEY_KEY = "ROEHHBigQN74jkFPxC5L1tG1D";
+    public static final String CONSUMER_SECRET_KEY = "dI9BcfHUYK5gomRUZNKcay8yJ3n6WPwPF74HNa592ZrRhuI2Lz";
+    public static final String ACCESS_TOKEN_KEY = "606182375-T9xlZTGYnUW6M914EKFk8XdKPfSKMyhiIR3r8hac";
+    public static final String ACCESS_TOKEN_SECRET_KEY = "FrkaJG4IEgWcSfTlNwsbs2TUhtN7PMwyr9eyPaIyXPpmg";
+}
